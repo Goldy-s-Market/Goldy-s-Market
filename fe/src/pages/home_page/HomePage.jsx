c64c3f8672151b6fa47ec1af0ee7b0342dd99a89
MouryaYandra <yandr008@umn.edu>
2025-10-01 20:52:24 -0500

    Tried adding Product Card design

--- fe/src/pages/home_page/HomePage.jsx
+++ fe/src/pages/home_page/HomePage.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
-
+import Card from "../products_page/CardPage";
 function HomePage() {
   return (
     <div>
       <h1 className="underline">This will be the home page</h1>
+      <div>
+        <Card></Card>
+      </div>
     </div>
   );
 }
