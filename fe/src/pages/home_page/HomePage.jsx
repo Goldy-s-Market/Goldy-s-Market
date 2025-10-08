af8f4c6d5ffd7ea0a3622ec78d16dad5070314fd
Darius Amenuvor <daarius24@gmail.com>
2025-10-08 16:09:57 -0500

    started the exo-skeleton of the homepage

--- fe/src/pages/home_page/HomePage.jsx
+++ fe/src/pages/home_page/HomePage.jsx
@@ -3,7 +3,32 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <div>
-      <h1 className="underline">This will be the home page</h1>
+      <header>
+        <div>
+          {/*Right header buttons */}
+          <div >
+            {/* Button components go here */}
+          </div>
+          {/*Left logo container */}
+          <div>
+            {/*Image goes here */}
+            <h2>Goldy's Market</h2>
+          </div>
+        </div>
+      </header>
+      
+      <main id="hero-section">
+        <h1>Buy, Sell, Trade with
+          <br>Fellow Students</br></h1>
+        <p>The trusted marketplace designed exclusively for University of Minnesota students.
+          Connect with you campus community to buy, sell, and bid on everything you need for student life.
+        </p>
+        {/*Call to action buttons */}
+        <div></div>
+        {/*Searching tool */}
+        <div id="Credibility-section"></div>
+
+      </main>
     </div>
   );
 }
