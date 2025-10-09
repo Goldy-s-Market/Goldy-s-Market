3d644837e30a1829546f953dea2cfa7d1665f476
Darius Amenuvor <daarius24@gmail.com>
2025-10-08 22:57:12 -0500

    completed initial styling of homepage, changed up some responsive design but has some issues that need to be fixed (check the comments)

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,18 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { Outlet, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div>
-      {/* <nav className="p-4 border-b">
-        <Link to="/" className="mr-4">Home</Link>
-        <Link to="/messages" className="mr-4">Messages</Link>
-        <Link to="/login">Login</Link>
-      </nav> */}
-      <main className="p-6">
+      <main >
         <Outlet />
       </main>
     </div>
