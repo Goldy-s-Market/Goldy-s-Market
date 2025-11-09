277c1911bca1760cf782efc1556125482f1a914b
MouryaYandra <yandr008@umn.edu>
2025-11-09 11:00:19 -0600

    CircularText.jsx file has error with import. Seems to not recogniz e 'motion/react' file.

--- fe/src/pages/CircularText.jsx
+++ fe/src/pages/CircularText.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { motion, useAnimation, useMotionValue } from 'motion/react';
+import { motion, useAnimation, useMotionValue } from 'react';
 
 const getRotationTransition = (duration, from, loop = true) => ({
   from,
