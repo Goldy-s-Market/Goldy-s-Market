2dca2833bfc228a4cdebff2af4694120356b82cf
MouryaYandra <yandr008@umn.edu>
2025-11-09 12:12:57 -0600

    Fixed message window to be flexable

--- fe/src/pages/messages_page/MessagesPage.jsx
+++ fe/src/pages/messages_page/MessagesPage.jsx
@@ -156,7 +156,7 @@ function MessagesPage() {
   const selectedContactData = contacts.find(c => c.id === selectedContact);
   return (
     
-    <div className="bg-gray-50 h-screen overflow-hidden">
+    <div className="bg-gray-50 h-screen w-screen overflow-hidden">
       {/*Header */}
       <header id="header" className="bg-white border-b border=gray-200 h-16 flex items-center px-6 shadow-sm">
         <div className="flex items-center space-x-8">
